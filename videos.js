df10a22e7cd7f8f29aa35ab38da0370586138c5d
yuvraj029 <hackhogyathased@gmail.com>
2025-08-29 01:15:29 +0530

    1

--- videos.js
+++ videos.js
@@ -1,7 +1,7 @@
 const VIDEOS = [
-  { title: "Are You Serious Right Now Bro Green Screen - CreatorSet Green Screens (1080p, h264).mp4", src: "memes/videos/Are You Serious Right Now Bro Green Screen - CreatorSet Green Screens (1080p, h264).mp4" },
+  { title: "Are You Serious Right Now Bro .mp4", src: "memes/videos/Are You Serious Right Now Bro .mp4" },
   { title: "bhabhi wo.mp4", src: "memes/videos/bhabhi wo.mp4" },
-  { title: "Cat dancing and singing south indian song meme template - Memes Making (720p, h264).mp4", src: "memes/videos/Cat dancing and singing south indian song meme template - Memes Making (720p, h264).mp4" },
+  { title: "Cat dancing and singing south indian song.mp4", src: "memes/videos/Cat dancing and singing south indian song.mp4" },
   { title: "Cat Laughing At You.mp4", src: "memes/videos/Cat Laughing At You.mp4" },
   { title: "fir.mp4", src: "memes/videos/fir.mp4" },
   { title: "hizru boi.mp4", src: "memes/videos/hizru boi.mp4" },
